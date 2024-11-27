f42e47a0d2edca3abcae9ddf87047eff02336a9c
Jonatan Eklöv <jonatan.eklov@gmail.com>
2024-11-27 16:31:39 +0100

    Strukturerat i funktioner

--- Exercise 2/Ex2.R
+++ Exercise 2/Ex2.R
@@ -13,42 +13,23 @@ load("./Data/genome3.rdata")
 #head(reference)
 #length(reference)
 
-genome1 = genome1[1:1000,]
-reference = reference[1:1000]
-# genome1[1,2:5] = c(0,0,0,0)
-
-coverage <- rowSums(genome1[, 2:5])
-print(min(coverage))
-
-#head(coverage)
-#summary(coverage)
+genome1 <- genome1[1:1000,]
+genome2 <- genome2[1:1000,]
+genome3 <- genome3[1:1000,]
+reference <- reference[1:1000]
 
 
-coverage_x_axis = seq(1,1000)
-coverage_data = data.frame(Index = coverage_x_axis, Coverage = coverage[coverage_x_axis])
-ggplot(data=coverage_data, aes(x = Index, y = Coverage)) + geom_point() + labs(x="Position")
-# ggplot(data=coverage_data, aes(x = Index, y = Coverage)) + geom_point()
+Function_Doing_Stuffs(genome1,reference,1)
+Function_Doing_Stuffs(genome2,reference,2)
+Function_Doing_Stuffs(genome3,reference,3)
 
-
-genome1.length = nrow(genome1)
-reference_indices <- match(reference, colnames(genome1)[-1])
-matches = genome1[cbind(1:genome1.length, reference_indices+1)]
-mismatch_values = coverage - matches
-mismatch_indices = which(mismatch_values != 0)
-mismatches <- data.frame(
-  Index = mismatch_indices,
-  Mismatch = mismatch_values[mismatch_indices],
-  Proportion = mismatch_values[mismatch_indices] / coverage[mismatch_indices]
-)
-
-dim(mismatches)
-#ggplot(data = mismatches, aes(x = Index, y = Proportion)) + geom_point()
-#ggsave("./Figures/proportion_missing_g1.png")
-
-
-#Binomial test
-mismatches$Pvalue <- numeric(nrow(mismatches))
-
+Plot_Coverage = function(coverage,genome_number){
+  coverage_x_axis <- seq(1,1000)
+  coverage_data <- data.frame(Index = coverage_x_axis, Coverage = coverage[coverage_x_axis])
+  ggplot(data=coverage_data, aes(x = Index, y = Coverage)) + geom_area() + labs(x="Position")
+  filename <- sprintf("./Figures/coverage%d.png", genome_number)
+  ggsave(filename)
+}
 
 Binom_calc = function(x,n){
   bin_result <- binom.test(x,n,p=0.1,alternative = "greater")
@@ -56,15 +37,53 @@ Binom_calc = function(x,n){
 }
 
 
-for (i in 1:nrow(mismatches)){
-    #bin_result <- binom.test(mismatches$Mismatch[i],coverage[mismatches$Index[i]],p=0.1,alternative = "greater")
-    mismatches$Pvalue[i] <- Binom_calc(mismatches$Mismatch[i],coverage[mismatches$Index[i]])
-    #mismatches$Pvalue[i] <- bin_result$p.value
+Function_Doing_Stuffs = function(genomex,reference,genome_number){
+  coverage <- rowSums(genomex[, 2:5]) 
+  Plot_Coverage(coverage,genome_number)
+  
+  genomex.length <- nrow(genomex)
+  
+  reference_indices <- match(reference, colnames(genomex)[-1])
+  matches = genomex[cbind(1:genomex.length, reference_indices+1)] 
+  
+  mismatch_values <- coverage - matches
+  mismatch_indices <- which(mismatch_values != 0)
+  
+  mismatches <- data.frame(
+    Index = mismatch_indices,
+    Mismatch = mismatch_values[mismatch_indices],
+    Proportion = mismatch_values[mismatch_indices] / coverage[mismatch_indices],
+    Coverage = coverage[mismatch_indices]
+  )
+  
+  # Plot proportion of mismatches
+  ggplot(data = mismatches, aes(x = Index, y = Proportion)) + geom_point()
+  filename <- sprintf("./Figures/proportion_missing%d.png", genome_number)
+  ggsave(filename)
+  
+  #Binomial test
+  mismatches$Pvalue <- numeric(nrow(mismatches))
+  for (i in 1:nrow(mismatches)){
+    mismatches$Pvalue[i] <- Binom_calc(mismatches$Mismatch[i],mismatches$Coverage[i])
+    
+  }
+  
+  #Plot P-values of mismatches
+  ggplot(data=mismatches, aes(x = Index, y = Pvalue)) + geom_point() 
+  filename <- sprintf("./Figures/pvalue_plot%d.png", genome_number)
+  ggsave(filename)
+  
+  mismatches <- mismatches[order(mismatches$Pvalue),]
+  
+  
 }
 
-plot(mismatches$Index,mismatches$Pvalue)
 
-mismatches <- mismatches[order(mismatches$Pvalue),]
+
+
+
+
+
 
 ######################
 
